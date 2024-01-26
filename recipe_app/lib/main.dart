31364c7fc679ebbffce29a799563b30921f91717
ITDhruv <120175094+ITDhruv@users.noreply.github.com>
2024-01-26 18:30:47 +0530

    🎉 Initital Commit

---
+++ recipe_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Recipe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        )
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
+
